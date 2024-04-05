1d930d705efc51270a36ed0b39c3fa8e5b1bfb39
Fra619-ghost <fxaaguilera@uamv.edu.ni>
2024-04-04 21:32:45 -0600

    Se agrega un segunda carpeta

---
+++ FrancoR22/jndjooos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    char name[20] = {"Francosi"};
+    char letter = {'F'};
+    printf("Su nombre es:%s",name);
+    printf("Su letra inicial es:%c", letter);
+    return 0;
+}
