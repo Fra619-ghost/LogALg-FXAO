3ce5c25c1598d035f0faf4604ac9e36eff9a87e5
Fra619-ghost <fxaaguilera@uamv.edu.ni>
2024-05-23 18:48:06 -0600

    Se agrega la sesion 21 de mayo

--- ejemplo1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
- main(int argc, char const *argv[])
-{
-    printf("Hola perro"); 
-    printf("/nMi nombre es: Franco");  //otro ejemplo
-    
-}
---
+++ sesion21May.cpp/variables.h
@@ -0,0 +1,15 @@
+
+typedef struct PERSONA{  //Tipo de dato en el que se puede almacenar varios tipos de datos a la vez
+    char name[100];  
+    char apellidos[100];
+    char cif[100];  
+    int  notas;  
+                                            /*Sintaxis
+                                            typedef -Struct - nombre de la variable-variables y sus tipos*/
+};       
+/*Una estrcutura es un conjunto de elementos que se relacionan entre si, es decir no vamos a poner variables que pertenezcan a 
+otros apartados*/
+
+//Abstraccion= de todos lo elememntos que tiene algo, solo sacamos los que nos interesan
+
+
